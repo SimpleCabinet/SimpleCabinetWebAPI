ca041712ac93d4862ac0ebf1bdc582770cdea286
KAPAMEJlbKA <promike96@gmail.com>
2024-04-05 15:17:41 +0300

    add sort

--- src/main/java/pro/gravit/simplecabinet/web/controller/NewsController.java
+++ src/main/java/pro/gravit/simplecabinet/web/controller/NewsController.java
@@ -24,7 +24,7 @@ public class NewsController {
 
     @GetMapping("/page/{pageId}")
     public PageDto<NewsDto> getPage(@PathVariable int pageId) {
-        var list = newsService.findAll(PageRequest.of(pageId, 10, Sort.Direction.ASC));
+        var list = newsService.findAll(PageRequest.of(pageId, 10, Sort.Direction.ASC, "id"));
         return new PageDto<>(list.map(NewsDto::makeMiniNews));
     }
 
@@ -47,6 +47,7 @@ public class NewsController {
         news.setHeader(request.header);
         news.setMiniText(request.miniText);
         news.setText(request.text);
+        news.setPicture(request.pictureName);
         newsService.save(news);
     }
 
@@ -68,6 +69,7 @@ public class NewsController {
             throw new EntityNotFoundException("News not found");
         }
         var comment = new NewsComment();
+        comment.setNews(news.get());
         comment.setUser(userService.getCurrentUser().getReference());
         comment.setText(createCommentRequest.text);
         newsService.save(comment);
