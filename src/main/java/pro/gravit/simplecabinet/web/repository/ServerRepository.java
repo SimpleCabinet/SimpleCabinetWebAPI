5c39cb4b2cec57d787d391ccf064bf4e7a1521e7
KAPAMEJlbKA <promike96@gmail.com>
2024-04-15 22:41:41 +0300

    Fix Ping

--- src/main/java/pro/gravit/simplecabinet/web/repository/ServerRepository.java
+++ src/main/java/pro/gravit/simplecabinet/web/repository/ServerRepository.java
@@ -1,7 +1,9 @@
 package pro.gravit.simplecabinet.web.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 import pro.gravit.simplecabinet.web.model.Server;
 
 import java.time.LocalDateTime;
@@ -11,7 +13,9 @@ import java.util.Optional;
 public interface ServerRepository extends JpaRepository<Server, Long> {
     Optional<Server> findByName(String name);
 
+    @Modifying
     @Query("update Server s set s.online = ?2, s.maxOnline = ?3, s.tps = ?4, s.users = ?5, s.updateDate = ?6 where s.name = ?1")
+    @Transactional
     void ping(String name, int online, int maxOnline, int tps, List<String> users, LocalDateTime updateDate);
 
     @Query("update Server s set s.online = ?2, s.maxOnline = ?3, s.tps = ?4, s.users = ?5, s.updateDate = ?6 where s.id = ?1")
