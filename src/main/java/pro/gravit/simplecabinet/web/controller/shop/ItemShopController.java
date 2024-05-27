8154a58fffa72f162d1d15d55a7ca860aecedb8b
KAPAMEJlbKA <promike96@gmail.com>
2024-05-27 18:15:14 +0300

    Add Search Items and Groups

--- src/main/java/pro/gravit/simplecabinet/web/controller/shop/ItemShopController.java
+++ src/main/java/pro/gravit/simplecabinet/web/controller/shop/ItemShopController.java
@@ -5,6 +5,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import pro.gravit.simplecabinet.web.dto.PageDto;
+import pro.gravit.simplecabinet.web.dto.shop.GroupProductDto;
 import pro.gravit.simplecabinet.web.dto.shop.ItemOrderDto;
 import pro.gravit.simplecabinet.web.dto.shop.ItemProductDto;
 import pro.gravit.simplecabinet.web.exception.EntityNotFoundException;
@@ -12,6 +13,7 @@ import pro.gravit.simplecabinet.web.exception.InvalidParametersException;
 import pro.gravit.simplecabinet.web.model.shop.ItemProduct;
 import pro.gravit.simplecabinet.web.service.DtoService;
 import pro.gravit.simplecabinet.web.service.shop.item.ItemProductService;
+import pro.gravit.simplecabinet.web.service.shop.item.ItemSearchService;
 import pro.gravit.simplecabinet.web.service.user.UserService;
 
 @RestController
@@ -23,10 +25,23 @@ public class ItemShopController {
     private UserService userService;
     @Autowired
     private DtoService dtoService;
+    @Autowired
+    private ItemSearchService searchit;
 
     @GetMapping("/page/{pageId}")
     public PageDto<ItemProductDto> getPage(@PathVariable int pageId) {
         var list = productService.findAllAvailable(PageRequest.of(pageId, 10));
+        return new PageDto<>(list.map(dtoService::toItemProductDto));
+    }
+
+    @GetMapping("/search/{data}/{pageId}")
+    public PageDto<ItemProductDto> searchByData(@PathVariable String data, @PathVariable int pageId ) {
+
+        var page = PageRequest.of(pageId, 10);
+        var list = searchit.findByDisplayName(data, page);
+        if (list.isEmpty()) {
+            throw new EntityNotFoundException("User not found");
+        }
         return new PageDto<>(list.map(dtoService::toItemProductDto));
     }
 
--- src/main/java/pro/gravit/simplecabinet/web/repository/shop/GroupSearchRepository.java
+++ src/main/java/pro/gravit/simplecabinet/web/repository/shop/GroupSearchRepository.java
@@ -1,4 +1,13 @@
 package pro.gravit.simplecabinet.web.repository.shop;
 
-public interface GroupSearchRepository {
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import pro.gravit.simplecabinet.web.model.shop.GroupProduct;
+
+public interface GroupSearchRepository extends JpaRepository<GroupProduct, Long> {
+    @Query("SELECT gp FROM GroupProduct gp WHERE gp.displayName ilike %:displayName%")
+    Page<GroupProduct> findByLocalName(@Param("displayName") String localName,Pageable pageable);
 }
