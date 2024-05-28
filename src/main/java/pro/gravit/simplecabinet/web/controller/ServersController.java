1a4094907a411d4012344e5a2bf867d457411c34
KAPAMEJlbKA <promike96@gmail.com>
2024-05-28 23:56:29 +0300

    Update ServersController.java

--- src/main/java/pro/gravit/simplecabinet/web/controller/ServersController.java
+++ src/main/java/pro/gravit/simplecabinet/web/controller/ServersController.java
@@ -63,6 +63,7 @@ public class ServersController {
     }
 
     @PostMapping("/name/{name}/ping")
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
     public void pingByName(@PathVariable String name, @RequestBody PingRequest request) {
         service.ping(name, request.online(), request.maxOnline(), request.tps(), request.users());
     }
